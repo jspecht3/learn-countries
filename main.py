5e449313bbd6a53ac29ef12e75598d7ea4b5d4bf
Joseph Specht <jspecht3@illinois.edu>
2024-12-15 13:03:37 -0600

    adding rounds

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from lists import countries, asia, eu, af, na, sa, au, me, balkan
+from data import germany, switzerland, lithuania
 
 
 def str_eq(str1, list1):
     list2 = []
     for item in list1:
         list2.append(item.lower())
-    return str1 in list2
+    return str1.lower() in list2
 
 
 def other(str1, list1):
@@ -41,38 +42,58 @@ def inc_str(list1):
     return to_return[0:-2]
 
 
-def start(challenge):
-    score = 0
-    increment = 0
-    total = len(challenge)
+def start(region):
+    challenge = region.copy()
+    next_countries = challenge.copy()
 
-    for country in challenge:
-        increment += 1
+    scores = []
+    totals = []
 
-        guess = input(f"\nWhat is the capital of {
-                      country.name[0]}?\n  Guess: ")
-        correct = str_eq(guess, country.capital)
+    while len(challenge) > 0:
+        score = 0
+        increment = 0
 
-        if correct:
-            score += 1
-            others = other_str(guess, country.capital)
+        totals.append(len(challenge))
+        total = totals[-1]
 
-            print(f"  Correct! Current Score: {
-                score}/{increment} ({total})")
-            if 0 != len(others):
-                print(f"  Other capitals include: {others}")
-            continue
+        print(f"\n ----- Beginning Round {len(totals)} -----")
 
-        while (not correct):
-            to_type = inc_str(country.capital)
-            guess = input(f"Incorrect, type {
-                to_type} to continue.\n  Guess: ")
+        for country in challenge:
+            increment += 1
+
+            guess = input(f"\nWhat is the capital of {
+                          country.name[0]}?\n  Guess: ")
             correct = str_eq(guess, country.capital)
 
-        print(f"  Current Score: {score}/{increment} ({total})")
+            if correct:
+                score += 1
+                others = other_str(guess, country.capital)
+                next_countries.remove(country)
+
+                print(f"  Correct! Current Score: {
+                    score}/{increment} ({total})")
+                if 0 != len(others):
+                    print(f"  Other capitals include: {others}")
+                continue
+
+            while (not correct):
+                to_type = inc_str(country.capital)
+                guess = input(f"Incorrect, type {
+                    to_type} to continue.\n  Guess: ")
+                correct = str_eq(guess, country.capital)
+
+            print(f"  Current Score: {score}/{increment} ({total})")
+        scores.append(score)
+
+        print(f"\n *** Round {len(totals)} Score: {score}/{total} *** \n")
+        challenge = next_countries.copy()
+
+    print(f"\n ----- Quiz Completed in {len(totals)} Rounds -----\n")
+    for i in range(len(totals)):
+        print(f"  Round {i + 1} Score: {scores[i]}/{totals[i]}\n")
 
 
-region = input("Welcome! begin by selecting a region from the following:\n  countries, asia, europe (eu), africa (af), north america (na), south america (sa), oceania (au), middle east (me), or balkan. \n Region Selected: ")
+# region = input("Welcome! begin by selecting a region from the following:\n  countries, asia, europe (eu), africa (af), north america (na), south america (sa), oceania (au), middle east (me), or balkan. \n Region Selected: ")
 
 regions = [countries, asia, eu, af, na, sa, au, me, balkan]
 
@@ -87,13 +108,15 @@ names = []
 for name in regions:
     names.append(variable_name(name))
 
-in_names = region in names
+# in_names = region in names
 
-while not in_names:
-    region = input(" Enter a valid region: ")
-    in_names = region in names
+# while not in_names:
+#    region = input(" Enter a valid region: ")
+#    in_names = region in names
 
-print(f'--- Beginning {region} Capital Quiz ---')
+# print(f'--- Beginning {region} Capital Quiz ---')
 
-index = names.index(region)
-start(regions[index])
+# index = names.index(region)
+# start(regions[index])
+
+start([germany, lithuania, switzerland])
