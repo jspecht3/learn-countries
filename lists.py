99b09ac4b855e6261182544b76a7d1d85d25a8d3
Joseph F. Specht IV <jspecht3@illinois.edu>
2025-07-14 23:04:31 -0400

    update lists

--- lists.py
+++ lists.py
@@ -3,7 +3,7 @@ from data import every
 countries = [item for item in every if 'country' in item.tags]
 asia = [item for item in every if 'as' in item.tags]
 eu = [item for item in every if 'eu' in item.tags]
-af = [item for item in every if 'af' in item.tags]
+af = [item for item in every if 'af' in item.tags][20:30]
 na = [item for item in every if 'na' in item.tags]
 sa = [item for item in every if 'sa' in item.tags]
 au = [item for item in every if 'au' in item.tags]
